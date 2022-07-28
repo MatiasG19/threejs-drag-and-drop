79a1b3998796a82d32ea5caa7c5ae86633270afe
MatiasG19 <m.galusek@outlook.com>
2022-07-28 22:37:31 +0200

    Update

--- src/engine2D/dragControls.ts
+++ src/engine2D/dragControls.ts
@@ -1,12 +1,54 @@
 import * as THREE from 'three'
+import { EventDispatcher } from 'three'
 
+export class DragControls extends EventDispatcher {
 
-export class DragControls {
+    private raycaster = new THREE.Raycaster()
+    private selected!: THREE.Object3D
 
-    private raycaster!: THREE.Raycaster
-    private intersections: THREE.Intersection[] = []
+    private pointer!: THREE.Vector2
 
-    constructor() {
+    private objects!: THREE.Mesh[]
+    private camera!: THREE.Camera
+    private canvas!: HTMLCanvasElement
 
+    constructor(objects: THREE.Mesh[], camera: THREE.Camera, canvas: HTMLCanvasElement ) {
+        super();
+
+        this.objects = objects
+        this.camera = camera
+        this.canvas = canvas
+
+        this.activate()
+    }
+
+    private activate() {
+        this.canvas.addEventListener('pointerdown', this.pointerDown.bind(this))
+        this.canvas.addEventListener('pointermove', this.pointerMove.bind(this))
+        this.canvas.addEventListener('pointercancel', this.pointerCancel.bind(this))
+        this.canvas.addEventListener('pointerup', this.pointerCancel.bind(this))
+    }
+
+    private pointerDown(event: PointerEvent) {
+        this.updatePointer(event)
+
+        this.raycaster.setFromCamera(this.pointer, this.camera)
+        this.selected  = this.raycaster.intersectObjects(this.objects)[0].object
+        if(this.selected)
+            this.dispatchEvent({ type: 'dragstart', object: this.selected })
+    }
+
+    private pointerMove(event: PointerEvent) {
+        this.dispatchEvent({ type: 'drag', object: {} })
+    }
+
+    private pointerCancel(event: PointerEvent) {
+        this.dispatchEvent({ type: 'dragend', object: this.selected })
+
+    }
+
+    private updatePointer(event: PointerEvent) {
+        this.pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     }
 }
--- src/engine2D/engine.ts
+++ src/engine2D/engine.ts
@@ -1,9 +1,12 @@
 import * as THREE from 'three'
+import { DragControls } from './dragControls'
 
 export class Engine2D {
     private renderer!: THREE.WebGLRenderer
     private scene!: THREE.Scene
     private camera!: THREE.OrthographicCamera
+    private objects!: THREE.Mesh[]
+    private dragControls!: DragControls
 
     constructor(canvas: HTMLCanvasElement) {
         // Scene
@@ -22,17 +25,40 @@ export class Engine2D {
         // Box
         const geometry = new THREE.BoxGeometry(400, 400, 400)
         const material = new THREE.MeshBasicMaterial( { color: 0x0000ff })
-        const box = new THREE.Mesh(geometry, material);
+        const box = new THREE.Mesh(geometry, material)
         this.scene.add(box)
+        this.objects.push(box)
+
+        // Enable drag controls
+        this.enableDragControls()
 
         // Renderer
         this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
         this.renderer.setPixelRatio(window.devicePixelRatio)
         this.renderer.setSize(window.innerWidth, window.innerHeight)
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera)
+    }
+
+    private enableDragControls() {
+        this.dragControls = new DragControls(this.objects, this.camera, this.renderer.domElement)
+        this.dragControls.addEventListener('dragstart', this.onPointerDown.bind(this))
+        this.dragControls.addEventListener('dragend', this.onPointerUp.bind(this))
+    }
+
+    private onPointerDown(event: THREE.Event) {
+        const object = this.objects.filter(o => o == event.object)[0]
+    }
+
+    private onPointerUp(event: THREE.Event) {
+
     }
 
     private resize() {
 
+    }
+
+    private dispose() {
+        this.dragControls.removeEventListener('dragstart', () => this.onPointerDown.bind(this))
+        this.dragControls.removeEventListener('dragend', this.onPointerUp.bind(this))
     }
 }
